85f90dc8b7bacf6bc71fa20ca8d2e777e5ae30b8
Neil Quillen <neilpquillen@gmail.com>
2019-02-16 08:36:40 -0800

    air vent toggle
    also added pipe

--- Practice Game 1/Assets/Scripts/AirVent.cs
+++ Practice Game 1/Assets/Scripts/AirVent.cs
@@ -5,23 +5,43 @@ using UnityEngine;
 public class AirVent : MonoBehaviour
 {
 
+    public bool isToggled;
+    [SerializeField]
+    private bool ventOn;
+    public float ventToggleSpeed;
+    private float ventToggleCounter;
+    public GameObject ventParticle;
+
     private Player player;
 
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<Player>();
+        ventToggleCounter = ventToggleSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isToggled)
+        {
+            
+            ventToggleCounter -= Time.deltaTime;
+            
+            if(ventToggleCounter <= 0)
+            {
+                ventOn = !ventOn;
+                ventToggleCounter = ventToggleSpeed;
+                VentParticleToggle();
+            }
+            
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if(collision.gameObject.tag == "Player" && ventOn)
         {
             player.onAirVent = true;
             
@@ -35,4 +55,18 @@ public class AirVent : MonoBehaviour
             player.onAirVent = false;
         }
     }
+
+    private void VentParticleToggle()
+    {
+        if (ventOn)
+        {
+            ventParticle.SetActive(true);
+        }
+        if (!ventOn)
+        {
+            ventParticle.SetActive(false);
+
+        }
+    }
+
 }
